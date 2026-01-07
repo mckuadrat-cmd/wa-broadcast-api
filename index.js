d254f80f65cd0b529cbe417270b2c4ca4edb51a5
mckuadrat <mckuadratid@gmail.com>
2026-01-07 12:35:01 +0700

    Update index.js

--- index.js
+++ index.js
@@ -54,7 +54,8 @@ const pgPool = new Pool({
 // MIDDLEWARE
 // =====================
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 
 // upload memory (15MB)
 const upload = multer({
